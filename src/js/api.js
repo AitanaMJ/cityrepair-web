247cbf2eb1282d4c7bc0cbf34d852413fe42dd15
lukasjc120 <143965229+lukasjc120@users.noreply.github.com>
2026-03-11 17:21:29 -0300

    api

--- src/js/api.js
+++ src/js/api.js
@@ -1,37 +1,17 @@
-const API = "http://localhost:3000/api";
+const API_URL = "http://localhost:3000/api";
 
 export async function obtenerReportes() {
-  const res = await fetch(`${API}/reportes`);
+  const res = await fetch(`${API_URL}/reportes`);
   return await res.json();
 }
 
 export async function crearReporte(data) {
-  const res = await fetch(`${API}/reportes`, {
+  const res = await fetch(`${API_URL}/reportes`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify(data)
-  });
-
-  return await res.json();
-}
-
-export async function actualizarReporte(id, data) {
-  const res = await fetch(`${API}/reportes/${id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  });
-
-  return await res.json();
-}
-
-export async function eliminarReporte(id) {
-  const res = await fetch(`${API}/reportes/${id}`, {
-    method: "DELETE"
   });
 
   return await res.json();
