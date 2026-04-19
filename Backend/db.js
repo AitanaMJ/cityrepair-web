72ba0d67f52fcec72b9754d32cf73d1d43ad4436
lukasjc120 <143965229+lukasjc120@users.noreply.github.com>
2026-04-19 12:25:40 -0300

    db.js
    contraseña

--- Backend/db.js
+++ Backend/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "TU_PASSWORD",
+  password: "sqlpromo2026",
   database: "cityrepair"
 });
 
